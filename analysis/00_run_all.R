4c3c322f7396058f22bfc044b4950cce42baf859
Pascal Schneider <96p.schneider@gmail.com>
2026-02-25 13:43:50 +0100

    Create 00_run_all.R

---
+++ analysis/00_run_all.R
@@ -0,0 +1,11 @@
+source("R/source.R")
+# source("analysis/01_wrangling_ACi-TGlob_V1.0.R") # Run only if raw data available
+# source("analysis/02_get_climate_data.R") # Run only if raw data available
+source("analysis/03_model_sensitivity_analysis.R")
+source("analysis/04_isolated_processes.R")
+source("analysis/05_warming_trajectories.R")
+source("analysis/06_seasonality-of-traits.R")
+source("analysis/07_additional_analyses.R")
+source("analysis/08_aopt_pfts_comparison.R")
+source("analysis/09_gather_final_outputs.R")
+beepr::beep()
